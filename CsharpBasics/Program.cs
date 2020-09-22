bed8b8c71c58849b22230ff406771acf83904421
Srivatsan Seshadri <srivatsan.seshadri@pearson.com>
2020-09-22 11:05:46 +0530

    Implementing Console Functionalities.

--- CsharpBasics/Program.cs
+++ CsharpBasics/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
 namespace CsharpBasics
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Welcome to C#!");
+            Console.WriteLine("Welcome to C#!");
+
+            //Formatted Output
+            Console.WriteLine("{0} Welcome to {1}", "Harshni", "C#");
+            
+            //String Annotation.
+            Console.WriteLine($"{"Harshni"}, Welcome to {"C#"}");
+
+            Console.Write("Enter the Student Name:");
+            string studentName = Console.ReadLine();
+            Console.Write("Enter the Subject Name:");
+            string subjectName = Console.ReadLine();
+
+            //Formatted Output
+            Console.WriteLine("{0} Welcome to {1}", studentName, subjectName);
+
+            //String Annotation.
+            Console.WriteLine($"{studentName}, Welcome to {subjectName}");
+
+            Console.ReadKey();
         }
     }
 }
